275a3d5a1dc34ee8245349d48ab7dd402e2957ba
surajGurung <sgrg007@gmail.com>
2020-06-11 23:36:35 -0500

    initial commit

---
+++ mavenforPOI/src/test/java/org/example/LoginDetails.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class LoginDetails {
+    @Test
+    public void doDisplayLogin(){
+        System.out.println("suraj Gurung is displaying");
+    }
+}
