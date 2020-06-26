253d6bb3d62da81e393df49f82d344429c7bf5c4
surajGurung <sgrg007@gmail.com>
2020-06-26 10:05:48 -0500

    post person is added

--- surajSpringBoot/src/main/java/com/example/surajSpringBoot/dao/FakePersonDataAccess.java
+++ surajSpringBoot/src/main/java/com/example/surajSpringBoot/dao/FakePersonDataAccess.java
@@ -1,11 +1,13 @@
 package com.example.surajSpringBoot.dao;
 
 import com.example.surajSpringBoot.model.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccess implements PersonDAO {
 
     private static List<Person> DB = new ArrayList<>();
--- surajSpringBoot/src/main/java/com/example/surajSpringBoot/service/PersonService.java
+++ surajSpringBoot/src/main/java/com/example/surajSpringBoot/service/PersonService.java
@@ -2,11 +2,16 @@ package com.example.surajSpringBoot.service;
 
 import com.example.surajSpringBoot.dao.PersonDAO;
 import com.example.surajSpringBoot.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonService {
     private final PersonDAO personDAO;
 
-    public PersonService(PersonDAO personDAO) {
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDAO personDAO) {
         this.personDAO = personDAO;
     }
 
